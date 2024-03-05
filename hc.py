956b7effe02869af38c82f371907a12a7d1055ac
chadgrover <93070367+chadgrover@users.noreply.github.com>
2024-03-05 22:16:47 +0900

    fix: Declare global variable

--- hc.py
+++ hc.py
@@ -13,22 +13,23 @@ if len(sys.argv) == 3:
     url = str(sys.argv[1])
     listening_port = urlparse(url).port
     removed_from_list = False
-    interval_in_seconds = int(sys.argv[2])
 
     def make_get_request():
+        global removed_from_list
+        
         response = requests.get(url)
 
-        if response.status_code == 200 and removed_from_list:
+        if response.status_code == 200 and removed_from_list is True:
             requests.post(LB_URL, data={"port": listening_port})
             removed_from_list = False
 
-        elif response.status_code != 200 and not removed_from_list:
+        elif response.status_code != 200 and removed_from_list is False:
             requests.delete(LB_URL, data={"port": listening_port})
             removed_from_list = True
-
         else:
             pass
 
+    interval_in_seconds = int(sys.argv[2])
     set_interval(make_get_request, interval_in_seconds)
 
 else:
